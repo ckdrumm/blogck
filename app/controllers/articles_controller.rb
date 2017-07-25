9bb2ef3b152103b9cb277441a8f486cc8bd2b62f
ckdrum <ck_drummond@yahoo.com>
2017-07-25 15:24:52 +0000

    updated bootstrap gem file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
+  
   def index
     @articles = Article.all
   end
@@ -19,15 +22,12 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
       if @article.update(article_params)
        flash[:notice] = "Article was updated"
        redirect_to article_path(@article)
@@ -37,7 +37,6 @@ class ArticlesController < ApplicationController
   end
    
    def destroy
-     @article = Article.find(params[:id])
      @article.destroy
      flash[:notice] = "Articale was deleted"
      redirect_to articles_path
@@ -48,5 +47,10 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description)
   end
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+    
   
 end
