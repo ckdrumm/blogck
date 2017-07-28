01223ebfed398a6e5aaa92ecd7cae4fc6a61fdc8
ckdrum <ck_drummond@yahoo.com>
2017-07-28 18:17:46 +0000

    add user data to articles listing page, made everything lowercase

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true 
   validates :description, presence: true
-  validates :user_id, presence: true
+  #validates :user_id, presence: true
   
 end
